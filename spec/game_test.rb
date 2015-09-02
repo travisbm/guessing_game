6b44a3e58ae33a5aad45eee9adcb20a35e418ce8
Travis Montgomery <travisbm@gmail.com>
2015-09-02 10:13:04 -0500

    still working on test

--- spec/game_test.rb
+++ spec/game_test.rb
@@ -4,30 +4,30 @@ require_relative '../lib/game.rb'  # => true
 
 class GameTest < MiniTest::Test
   def test_check_low
-    game = Game.new              # => #<Game:0x007ff8bd0f6ff0 @num=36, @win=false, @low=false, @high=false>
+    game = Game.new              # => #<Game:0x007fd4b21e2920 @num=73, @win=false, @low=false, @high=false>
 
     @guess = 1     # => 1
     @num = 50      # => 50
     @low = false   # => false
     @high = false  # => false
 
-    assert_equal(@low = false, game.check)
+    assert_equal(@low == false, game.check)
   end
 end
 
 
-# >> Run options: --seed 41666
+# >> Run options: --seed 7916
 # >>
 # >> # Running:
 # >>
 # >> E
 # >>
-# >> Finished in 0.001089s, 917.9061 runs/s, 0.0000 assertions/s.
+# >> Finished in 0.001001s, 998.5561 runs/s, 0.0000 assertions/s.
 # >>
 # >>   1) Error:
 # >> GameTest#test_check_low:
 # >> NoMethodError: undefined method `<' for nil:NilClass
-# >>     /Users/Britton/theironyard/programs/guessing_game/lib/game.rb:43:in `check'
+# >>     /Users/Britton/theironyard/programs/guessing_game/lib/game.rb:36:in `check'
 # >>     /Users/Britton/theironyard/programs/guessing_game/spec/game_test.rb:14:in `test_check_low'
 # >>
 # >> 1 runs, 0 assertions, 0 failures, 1 errors, 0 skips
